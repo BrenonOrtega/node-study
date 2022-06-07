07514ccf04ed32d3375897774b7011c71850ecbe
Brenon Ortega <brenon.ortega@hotmail.com>
2022-06-07 20:29:38 -0300

    overloaded on/emit methods for additional type check.

---
+++ pluralsight/getting-started/3-async-and-events/emitting-events.ts
@@ -0,0 +1,24 @@
+import { EventEmitter } from "events";
+
+enum MyEmitterEvents {
+  TEST_EVENT = "TEST_EVENT",
+  INVOCATION = "INVOCATION",
+  END = "END"
+}
+
+class MyEmitter extends EventEmitter {
+  on(eventName: MyEmitterEvents, listener: (...args: any[]) => void): this {
+    return super.on(eventName, listener);
+  }
+
+  emit(eventName: MyEmitterEvents, ...args: any[]): boolean {
+    return super.emit(eventName, ...args);
+  }
+}
+const myEmitter: MyEmitter = new EventEmitter();
+
+myEmitter.emit(MyEmitterEvents.INVOCATION);
+
+myEmitter.on(MyEmitterEvents.INVOCATION, args => console.log(`invocation happened ${args ? `with args ${args}` : "with no args"}`));
+
+myEmitter.emit(MyEmitterEvents.INVOCATION, MyEmitterEvents.INVOCATION);
