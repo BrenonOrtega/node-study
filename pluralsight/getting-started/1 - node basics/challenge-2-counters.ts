49b940d167ad8ef3c14c0bde014be095e65b80a7
Brenon Ortega <brenon.ortega@hotmail.com>
2022-06-07 19:41:16 -0300

    Finishes Module 1-basics  & 4 coding challenges.

--- pluralsight/getting-started/1 - node basics/challenge-2-counters.ts
+++ pluralsight/getting-started/1 - node basics/challenge-2-counters.ts
@@ -1,12 +1,14 @@
 import timer from "timers";
 
 let occurences: number = 0;
-
-const timerId = timer.setInterval(() => {
+const greetFiveTimes = () => {
   console.log(`Hello world`);
   occurences++;
 
   if(occurences >= 5){
+    console.log("Done");
     clearInterval(timerId);
   } 
-});
+};
+
+const timerId = timer.setInterval(greetFiveTimes, 1000);
