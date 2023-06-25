d1acc318a48001612bac9274ff8f91ec71061461
BrenonOrtega <brenon.ortega@hotmail.com>
2023-06-25 18:46:54 +0200

    Refactors App component for new functionalities

--- react-fundamentals/src/App.js
+++ react-fundamentals/src/App.js
@@ -3,12 +3,16 @@ import BlogArticle from './Components/BlogArticle';
 import Header from './Header'
 import createReducerFor from './Functions/createReducer';
 import { articlesSeed } from './articlesSeed';
-import { appendItem, sum, } from './Functions';
+import { appendItem, sum, getNewId, filterById } from './Functions';
+import { createAction } from './Functions/createActions';
+
 
 const actions = {
-  refresh: 'refreshArticles',
-  childrenContent: 'receiveChildrenContent',
-  increaseCount: 'increaseCount'
+  refreshArticles: createAction('refreshArticles', appendItem),
+  removeArticle: createAction('removeArticle', filterById),
+  childrenContent: createAction('receiveChildrenContent'),
+  increaseCount: createAction('increaseCount', sum),
+
 };
 
 const reducer = createReducerFor(actions);
@@ -16,16 +20,14 @@ const reducer = createReducerFor(actions);
 const App = () => {
   const [ state, dispatch ] = useReducer(reducer, { articles: articlesSeed, childrenContent: '', count: 0 });
 
-  const handleRefreshArticle = () => {
-    const length = state.articles.length;
-    dispatch({ action: actions.childrenContent, key: 'articles', value: createArticle(length), applyTransform: appendItem, });
-  };
+  const handleRefreshArticle = 
+    () => dispatch({ action: actions.refreshArticles, key: 'articles', value: createArticle(state.articles.length) });
 
-  const handleIncreaseClick =
-    () => dispatch({ action: actions.increaseCount, key: 'count', value: 1, applyTransform: sum });
+  const handleIncreaseClick = () => dispatch({ action: actions.increaseCount, key: 'count', value: 1 });
 
-  const receiveContentChanged =
-    (received) => dispatch({ action: actions.childrenContent, key: 'childrenContent', value: received });
+  const receiveContentChanged =(received) => dispatch({ action: actions.childrenContent, key: 'childrenContent', value: received });
+
+  const handleRemoveArticle = (articleId) => dispatch({ action: actions.removeArticle, key: 'articles', value: articleId });
 
   return <Fragment key="fragment">
     <Header title="My blog articles" content={state.childrenContent} >
@@ -33,17 +35,18 @@ const App = () => {
       <h5>{state.childrenContent}</h5>
     </Header>
 
-    <button onClick={handleIncreaseClick}>Clicked {state.count} times
-    </button>
+    <button onClick={handleIncreaseClick}>Clicked {state.count} times</button>
     <button onClick={handleRefreshArticle}>Refresh Articles</button>
 
     {state.articles.map(article =>
       <BlogArticle
+        id={article.id}
         key={article.title}
         title={article.title}
         subtitle={article.subtitle}
         content={article.content}
         onContentChanged={receiveContentChanged}
+        onRemoveArticle={handleRemoveArticle}
       />)
     }
   </Fragment>;
@@ -53,6 +56,7 @@ export default App;
 
 function createArticle(length) {
   return {
+    id: getNewId(),
     title: `Article number ${length}`,
     subtitle: `SubArticle#${length}`,
     content: "eeeeeeeeeeeu, parado no bailao UUUH no bailao, ela com o popozao"
