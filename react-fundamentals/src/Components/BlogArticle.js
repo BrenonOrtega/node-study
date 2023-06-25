42b76ff3ce59a9586b7be8be0fb6b7fd1e3e5e62
BrenonOrtega <brenon.ortega@hotmail.com>
2023-06-25 18:44:45 +0200

    Adds remove button for BlogArticle

--- react-fundamentals/src/Components/BlogArticle.js
+++ react-fundamentals/src/Components/BlogArticle.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import propTypes from 'prop-types';
 
-const BlogArticle = ({ title, subtitle, content, children, onContentChanged }) => {
+const BlogArticle = ({ id, title, subtitle, content, children, onContentChanged, onRemoveArticle }) => {
   
   const [ newContent, setNewContent ] = useState('');
   const handleContentButtonClick = () => setNewContent(`${title} content has been clicked`);
+  const handleRemoveButtonClick = () => onRemoveArticle(id);
   useEffect(() => onContentChanged?.(newContent), [ newContent ]);
   
   const renderContentIfExists = () => newContent ? <p>{newContent}</p> : '';
   return (
     <article>
-      <strong>{title}</strong><br></br>
+      <strong>{title} <button onClick={handleRemoveButtonClick}>Remove</button></strong><br></br>
       <medium>{subtitle}</medium>
       <br />
       <p>{content}</p>
@@ -23,10 +24,12 @@ const BlogArticle = ({ title, subtitle, content, children, onContentChanged }) =
 };
 
 BlogArticle.propTypes = {
+  id: propTypes.number.isRequired,
   title: propTypes.string.isRequired,
   subtitle: propTypes.string.isRequired,
   content: propTypes.string.isRequired,
   onContentChanged: propTypes.func,
+  onRemoveArticle: propTypes.func,
 };
 
 export default BlogArticle;
