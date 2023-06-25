0dbc5aadf51337c73569a17b4c08d90ca7680c94
BrenonOrtega <brenon.ortega@hotmail.com>
2023-06-25 15:06:54 +0200

    Refactors BlogArticle for legitibility

--- react-fundamentals/src/Components/BlogArticle.js
+++ react-fundamentals/src/Components/BlogArticle.js
@@ -2,21 +2,20 @@ import React, { useEffect, useState } from 'react';
 import propTypes from 'prop-types';
 
 const BlogArticle = ({ title, subtitle, content, children, onContentChanged }) => {
-
+  
   const [ newContent, setNewContent ] = useState('');
-
+  const handleContentButtonClick = () => setNewContent(`${title} content has been clicked`);
   useEffect(() => onContentChanged?.(newContent), [ newContent ]);
-
+  
+  const renderContentIfExists = () => newContent ? <p>{newContent}</p> : '';
   return (
     <article>
       <strong>{title}</strong><br></br>
       <medium>{subtitle}</medium>
       <br />
       <p>{content}</p>
-      {newContent ? <p>{newContent}</p> : ''}
-      <button onClick={() => setNewContent(`${title} content has been clicked`)}>
-        Click for new content
-      </button>
+      {renderContentIfExists()}
+      <button onClick={handleContentButtonClick}>Click for new content</button>
       <hr></hr>
       {children}
     </article>
